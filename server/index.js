5c8b93a989f5bf216b689b4e63d9fc2faacef50f
15153246027 <652992429@qq.com>
2020-05-14 13:40:50 +0800

    优化基本配置

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ async function start () {
 
   const {
     host = process.env.HOST || '127.0.0.1',
-    port = process.env.PORT || 3000
+    port = process.env.PORT || 3004
   } = nuxt.options.server
 
   await nuxt.ready()
